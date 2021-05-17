19f14a5433d5347f88dd1a498803b802b46a8d73
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-05-17 11:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+let newHeader = document.getElementById('victory');
+// newHeader.innerHTML = "<p>YOUR-NAME is the champion</p>";
